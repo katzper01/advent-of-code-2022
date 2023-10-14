831bd797d75cd9280069c288afd694fe37a5afd7
katzper01 <katzper.michno@gmail.com>
2023-10-14 22:56:32 +0200

    Day2/Part1

---
+++ 2/2A.rs
@@ -0,0 +1,35 @@
+// Advent of Code 2022
+// Day 2/Part 1
+// https://adventofcode.com/2022/day/2
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines();
+    
+    let mut result: u32 = 0;
+
+    for line in lines {
+        let symbols: Vec<char> = line
+            .unwrap()
+            .split(" ")
+            .map(str::to_string)
+            .map(|s| s.chars().next().expect("string is empty"))
+            .collect();
+
+        let they: u32 = (*symbols.get(0).unwrap() as u32) - ('A' as u32);
+        let me: u32 = (*symbols.get(1).unwrap() as u32) - ('X' as u32);
+
+        result += me + 1;
+
+        if me == they {
+            result += 3;
+        } else if (me + 2) % 3 == they {
+            result += 6;
+        }
+    }
+
+    println!("{}", result);
+
+    Ok(())
+}
