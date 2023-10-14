47eca305eaeaef019530917539367acd08231857
katzper01 <katzper.michno@gmail.com>
2023-10-14 22:08:10 +0200

    Day 1

---
+++ 1/1A.rs
@@ -0,0 +1,29 @@
+// Advent of Code 2022
+// Day 1/Part 1
+// https://adventofcode.com/2022/day/1
+use std::io::{self, BufRead};
+use std::cmp;
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines();
+
+    let mut result: i32 = 0;
+    let mut sum: i32 = 0;
+
+    for line in lines {
+        let content: String = line.unwrap();
+
+        if content.len() > 0 {
+            let calories = content.parse::<i32>().unwrap();
+            sum += calories;
+        } else {
+            result = cmp::max(result, sum);
+            sum = 0;
+        }
+    }
+
+    println!("{}", result);
+
+    Ok(())
+}
---
+++ 1/1B.rs
@@ -0,0 +1,36 @@
+// Advent of Code 2022
+// Day 1/Part 2
+// https://adventofcode.com/2022/day/1
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines();
+
+    let mut sum: i32 = 0;
+    let mut v: Vec<i32> = Vec::new();
+
+    for line in lines {
+        let content: String = line.unwrap();
+
+        if content.len() > 0 {
+            let calories = content.parse::<i32>().unwrap();
+            sum += calories;
+        } else {
+            v.push(sum);
+            sum = 0;
+        }
+    }
+
+    v.sort();
+
+    let mut result: i32 = 0;
+
+    for i in 1..4 { 
+        result += v.get(v.len() - i).unwrap();
+    }
+
+    println!("{}", result);
+
+    Ok(())
+}
